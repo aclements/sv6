407f758fb3f4222258a298cb888a67204090fa26
Austin Clements <amdragon@mit.edu>
2012-06-08 18:14:28 -0400

    Make atomic_std.h play nicely with other host headers
    Previously we defined several things from c++config.h ourselves, which
    conflicted with the real c++config.h when we used host headers.  Now
    that we have a real c++config.h, include it from atomic_base.h.
    
    Unfortunately, we can't just switch to the host atomic header since
    we've made our own bug fixes to these headers.  However, it's likely
    that these problems have been fixed in GCC 4.7.

--- include/atomic.hh
+++ include/atomic.hh
@@ -1,17 +1,6 @@
 #pragma once
 
-#define _GLIBCXX_VISIBILITY(x)
-#define _GLIBCXX_BEGIN_NAMESPACE_VERSION
-#define _GLIBCXX_END_NAMESPACE_VERSION
-#define _GLIBCXX_BEGIN_EXTERN_C extern "C" {
-#define _GLIBCXX_END_EXTERN_C }
-#define __glibcxx_assert(x)
 #define __barrier() do { __asm__ __volatile__("" ::: "memory"); } while (0)
-
-#define _GLIBCXX_ATOMIC_BUILTINS_1 1
-#define _GLIBCXX_ATOMIC_BUILTINS_2 1
-#define _GLIBCXX_ATOMIC_BUILTINS_4 1
-#define _GLIBCXX_ATOMIC_BUILTINS_8 1
 
 #include "atomic_std.h"
 #include "atomic_util.hh"
