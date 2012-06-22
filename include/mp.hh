688c416c4c7aba4519fd775ac9ebf79cc00111b1
Austin Clements <amdragon@mit.edu>
2012-06-21 23:16:34 -0400

    mp: Clean up and flesh out MP table code
    Implement more complete handling for the MP table, including more
    sanity checks and better error reporting.  Improve comments, including
    references to the MP spec.  Make the code more type-safe.  Add lots of
    debugging output.

--- include/mp.hh
+++ include/mp.hh
@@ -1,49 +1,70 @@
 // See MultiProcessor Specification Version 1.[14]
 
-struct mp {             // floating pointer
+struct mp {             // floating pointer [MP 4.1]
   u8 signature[4];           // "_MP_"
-  u32 physaddr;               // phys addr of MP config table
+  u32 physaddr;              // phys addr of MP config table
   u8 length;                 // 1
   u8 specrev;                // [14]
   u8 checksum;               // all bytes must add up to 0
   u8 type;                   // MP system config type
   u8 imcrp;
   u8 reserved[3];
-};
+} __attribute__((__packed__));
 
-struct mpconf {         // configuration table header
-  u8 signature[4];           // "PCMP"
+struct mpconf {         // configuration table header [MP 4.2]
+  char signature[4];         // "PCMP"
   u16 length;                // total table length
   u8 version;                // [14]
   u8 checksum;               // all bytes must add up to 0
-  u8 product[20];            // product id
-  u32 oemtable;               // OEM table pointer
+  char oemid[8];             // OEM ID string
+  char product[12];          // product id
+  u32 oemtable;              // OEM table pointer
   u16 oemlength;             // OEM table length
   u16 entry;                 // entry count
-  u32 lapicaddr;              // address of local APIC
+  u32 lapicaddr;             // address of local APIC
   u16 xlength;               // extended table length
   u8 xchecksum;              // extended table checksum
   u8 reserved;
-};
+  u8 entries[0];             // table entries
+} __attribute__((__packed__));
 
-struct mpproc {         // processor table entry
+struct mpproc {         // processor table entry [MP 4.3.1]
   u8 type;                   // entry type (0)
   u8 apicid;                 // local APIC id
-  u8 version;                // local APIC verison
+  u8 version;                // local APIC version
   u8 flags;                  // CPU flags
-    #define MPBOOT 0x02           // This proc is the bootstrap processor.
+    #define MPPROC_EN 0x01        // Enabled
+    #define MPPROC_BP 0x02        // This proc is the bootstrap processor.
   u8 signature[4];           // CPU signature
-  u32 feature;                 // feature flags from CPUID instruction
+  u32 feature;               // feature flags from CPUID instruction
   u8 reserved[8];
-};
+} __attribute__((__packed__));
 
-struct mpioapic {       // I/O APIC table entry
+struct mpbus {          // bus entry [MP 4.3.2]
+  u8 type;                   // entry type (1)
+  u8 busid;                  // bus id
+  char name[6];              // bus type string
+} __attribute__((__packed__));
+
+struct mpioapic {       // I/O APIC table entry [MP 4.3.3]
   u8 type;                   // entry type (2)
-  u8 apicno;                 // I/O APIC id
+  u8 apicid;                 // I/O APIC id
   u8 version;                // I/O APIC version
   u8 flags;                  // I/O APIC flags
+    #define MPIOAPIC_EN 0x1       // Enable
   u32 addr;                  // I/O APIC address
-};
+} __attribute__((__packed__));
+
+struct mpint {          // I/O or local interrupt assignment entry [MP 4.3.4]
+  u8 type;                   // entry type (3=I/O, 4=Local)
+  u8 inttype;                // interrupt type
+                             // (0=INT, 1=NMI, 2=SMI, 3=ExtINT)
+  u16 flag;                  // bits: 0-1: PO; 2-3: EL
+  u8 busid;                  // source bus id
+  u8 busirq;                 // source bus irq
+  u8 apicno;                 // destination (L)APIC id
+  u8 pinno;                  // destination (L)APIC (L)INTIN#
+} __attribute__((__packed__));
 
 // Table entry types
 #define MPPROC    0x00  // One per processor
--- kernel/mp.cc
+++ kernel/mp.cc
@@ -7,6 +7,7 @@
 #include "mp.hh"
 #include "kernel.hh"
 #include "cpu.hh"
+#include "kstream.hh"
 
 struct cpu cpus[NCPU];
 static struct cpu *bcpu __mpalign__;
@@ -14,39 +15,85 @@ int ismp __mpalign__;
 int ncpu __mpalign__;
 u8 ioapicid __mpalign__;
 
+static console_stream verbose(true);
+
 int
 mpbcpu(void)
 {
   return bcpu-cpus;
 }
 
+void
+to_stream(print_stream *s, const mpconf &m)
+{
+  s->print("mpconf{signature=", sbuf(m.signature, 4), " length=", m.length,
+           " version=", m.version, " checksum=", shex(m.checksum),
+           " oemid=", sbuf(m.oemid, sizeof m.oemid),
+           " product=", sbuf(m.product, sizeof m.product),
+           " oemtable=", shex(m.oemtable),
+           " oemlength=", m.oemlength, " entry=", m.entry,
+           " lapicaddr=", shex(m.lapicaddr), " xlength=", m.xlength,
+           " xchecksum=", shex(m.xchecksum), "}");
+}
+
+void
+to_stream(print_stream *s, const mpproc &m)
+{
+  s->print("mpproc{apicid=", m.apicid, " version=", m.version,
+           " flags=", sflags(m.flags, {{"EN", MPPROC_EN}, {"BP", MPPROC_BP}}),
+           "}");
+}
+
+void
+to_stream(print_stream *s, const mpbus &m)
+{
+  s->print("mpbus{busid=", m.busid, " name=", sbuf(m.name, sizeof m.name), "}");
+}
+
+void
+to_stream(print_stream *s, const mpioapic &m)
+{
+  s->print("mpioacpi{apicid=", m.apicid, " version=", m.version,
+           " flags=", sflags(m.flags, {{"EN", MPIOAPIC_EN}}),
+           " addr=", shex(m.addr), "}");
+}
+
+void
+to_stream(print_stream *s, const mpint &m)
+{
+  s->print("mpint{type=", senum(m.type, {{"IO", 3}, {"local", 4}}),
+           " inttype=", senum(m.inttype, {"INT", "NMI", "SMI", "Ext"}),
+           " po=", senum(m.flag & 3, {"bus", "active-high", {"active-low", 3}}),
+           " el=", senum((m.flag >> 2) & 3, {"bus", "edge", {"level", 3}}),
+           " busid=", m.busid, " busirq=", m.busirq,
+           " apicno=", m.apicno, " pinno=", m.pinno, "}");
+}
+
 static u8
-sum(u8 *addr, int len)
+sum(void *addr, int len)
 {
   int i, sum;
   
   sum = 0;
-  for(i=0; i<len; i++)
-    sum += addr[i];
+  for(i = 0; i < len; i++)
+    sum += ((u8*)addr)[i];
   return sum;
 }
 
 // Look for an MP structure in the len bytes at addr.
 static struct mp*
-mpsearch1(u8 *addr, int len)
+mpsearch1(paddr addr, int len)
 {
-  u8 *e, *p;
+  struct mp *p = (mp*)p2v(addr), *e = (mp*)p2v(addr + len);
 
-  addr = (u8*) p2v((uptr) addr);
-  e = addr+len;
-  for(p = addr; p < e; p += sizeof(struct mp))
-    if(memcmp(p, "_MP_", 4) == 0 && sum(p, sizeof(struct mp)) == 0)
-      return (struct mp*)p;
-  return 0;
+  for (; p < e; ++p)
+    if (memcmp(p->signature, "_MP_", 4) == 0 && sum(p, sizeof(*p)) == 0)
+      return p;
+  return nullptr;
 }
 
-// Search for the MP Floating Pointer Structure, which according to the
-// spec is in one of the following three locations:
+// Search for the MP Floating Pointer Structure, which according to
+// [MP 4] is in one of the following three locations:
 // 1) in the first KB of the EBDA;
 // 2) in the last KB of system base memory;
 // 3) in the BIOS ROM between 0xE0000 and 0xFFFFF.
@@ -57,38 +104,66 @@ mpsearch(void)
   paddr p;
   struct mp *mp;
 
-  bda = (u8*)p2v(0x400);
-  if((p = ((bda[0x0F]<<8)|bda[0x0E]) << 4)){
-    if((mp = mpsearch1((u8*)p, 1024)))
+  // The BIOS Data Area lives in 16-bit segment 0x40.
+  bda = (u8*)p2v(0x40 << 4);
+
+  // [MP 4] The 16-bit segment of the EBDA is in the two bytes
+  // starting at byte 0x0E of the BDA.  0 if not present.
+  if ((p = ((bda[0x0F] << 8) | bda[0x0E]) << 4)) {
+    if ((mp = mpsearch1(p, 1024))) {
+      verbose.println("SMP: Found MP table in EBDA at ", mp);
       return mp;
+    }
   } else {
-    p = ((bda[0x14]<<8)|bda[0x13])*1024;
-    if((mp = mpsearch1((u8*)p-1024, 1024)))
+    // The size of base memory, in KB is in the two bytes starting at
+    // 0x13 of the BDA.
+    p = ((bda[0x14] << 8) | bda[0x13]) * 1024;
+    if ((mp = mpsearch1(p-1024, 1024))) {
+      verbose.println("SMP: Found MP table in system base memory at ", mp);
       return mp;
+    }
   }
-  return mpsearch1((u8*)0xF0000, 0x10000);
+  if ((mp = mpsearch1(0xF0000, 0x10000))) {
+    verbose.println("SMP: Found MP table in BIOS ROM at ", mp);
+    return mp;
+  }
+  return nullptr;
 }
 
 // Search for an MP configuration table.  For now,
 // don't accept the default configurations (physaddr == 0).
 // Check for correct signature, calculate the checksum and,
 // if correct, check the version.
-// To do: check extended table checksum.
 static struct mpconf*
 mpconfig(struct mp **pmp)
 {
   struct mpconf *conf;
   struct mp *mp;
 
-  if((mp = mpsearch()) == 0 || mp->physaddr == 0)
-    return 0;
+  if ((mp = mpsearch()) == 0)
+    return nullptr;
+  if (mp->physaddr == 0 || mp->type != 0) {
+    swarn.println("SMP: Default configurations not implemented");
+    return nullptr;
+  }
   conf = (struct mpconf*) p2v((uptr) mp->physaddr);
-  if(memcmp(conf, "PCMP", 4) != 0)
-    return 0;
-  if(conf->version != 1 && conf->version != 4)
-    return 0;
-  if(sum((u8*)conf, conf->length) != 0)
-    return 0;
+  if (memcmp(conf->signature, "PCMP", 4) != 0) {
+    swarn.println("SMP: Incorrect MP configuration table signature");
+    return nullptr;
+  }
+  if (sum(conf, conf->length) != 0) {
+    swarn.println("SMP: Bad MP configuration checksum");
+    return nullptr;
+  }
+  if (conf->version != 1 && conf->version != 4) {
+    swarn.println("SMP: Unsupported MP version ", conf->version);
+    return nullptr;
+  }
+  if ((sum((u8*)conf + conf->length, conf->xlength) + conf->xchecksum) & 0xFF) {
+    swarn.println("SMP: Bad MP configuration extended checksum ",
+                  shex(conf->xchecksum));
+    return nullptr;
+  }
   *pmp = mp;
   return conf;
 }
@@ -99,39 +174,54 @@ initmp(void)
   u8 *p, *e;
   struct mp *mp;
   struct mpconf *conf;
-  struct mpproc *proc;
-  struct mpioapic *ioapic;
 
   bcpu = &cpus[0];
-  if((conf = mpconfig(&mp)) == 0)
+  if ((conf = mpconfig(&mp)) == 0)
     return;
   ismp = 1;
 
-  for(p=(u8*)(conf+1), e=(u8*)conf+conf->length; p<e; ){
+  verbose.println(*conf);
+  for (p = conf->entries, e = (u8*)conf+conf->length; p < e; ) {
     switch(*p){
-    case MPPROC:
-      proc = (struct mpproc*)p;
-      if (ncpu == NCPU)
-        panic("initmp: too many CPUs");
-      if(proc->flags & MPBOOT)
-        bcpu = &cpus[ncpu];
-      cpus[ncpu].id = ncpu;
-      cpus[ncpu].hwid = HWID(proc->apicid);
-      ncpu++;
-      p += sizeof(struct mpproc);
+    case MPPROC: {
+      struct mpproc *proc = (struct mpproc*)p;
+      verbose.println(*proc);
+      if (proc->flags & MPPROC_EN) {
+        if (ncpu == NCPU)
+          panic("initmp: too many CPUs");
+        if(proc->flags & MPPROC_BP)
+          bcpu = &cpus[ncpu];
+        cpus[ncpu].id = ncpu;
+        cpus[ncpu].hwid = HWID(proc->apicid);
+        ncpu++;
+      }
+      p += sizeof(*proc);
       continue;
-    case MPIOAPIC:
-      ioapic = (struct mpioapic*)p;
-      ioapicid = ioapic->apicno;
-      p += sizeof(struct mpioapic);
+    }
+    case MPBUS: {
+      struct mpbus *bus = (struct mpbus*)p;
+      verbose.println(*bus);
+      p += sizeof(*bus);
       continue;
-    case MPBUS:
+    }
+    case MPIOAPIC: {
+      struct mpioapic *ioapic = (struct mpioapic*)p;
+      verbose.println(*ioapic);
+      if (ioapic->flags & MPIOAPIC_EN) {
+        ioapicid = ioapic->apicid;
+      }
+      p += sizeof(*ioapic);
+      continue;
+    }
     case MPIOINTR:
-    case MPLINTR:
-      p += 8;
+    case MPLINTR: {
+      struct mpint *in = (struct mpint*)p;
+      verbose.println(*in);
+      p += sizeof(*in);
       continue;
+    }
     default:
-      cprintf("mpinit: unknown config type %x\n", *p);
+      swarn.println("SMP: Unknown config type ", shex(*p));
       ismp = 0;
     }
   }
