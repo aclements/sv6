39c18319805b290f1238f6fbfe4dc77cdfa6b4cd
Austin Clements <amdragon@mit.edu>
2012-06-22 23:56:20 -0400

    condvar: Disable copy, enable move, and legitimize default ctor
    Technically the compiler already did the right things for copy and
    move methods because of the spinlock member, but this gives better
    error messages if a caller screws up.

--- include/condvar.h
+++ include/condvar.h
@@ -7,13 +7,24 @@ struct condvar {
   LIST_HEAD(waiters, proc) waiters;
 
 #ifdef __cplusplus
-  // Create a condvar that will later be initialized by initcondvar.
-  // XXX(austin) Remove this and initcondvar.
+  // Construct an uninitialized condvar.  This should be move-assigned
+  // from an initialized condvar before being used.  This is
+  // constexpr, so it can be used for global condvars without
+  // incurring a static constructor.
+  // XXX(austin) Remove initcondvar.
   constexpr condvar()
     : lock(), waiters{} { }
 
   constexpr
   condvar(const char *name)
     : lock(name, LOCKSTAT_CONDVAR), waiters{} { }
+
+  // Condvars cannot be copied.
+  condvar(const condvar &o) = delete;
+  condvar &operator=(const condvar &o) = delete;
+
+  // Condvars can be moved.
+  condvar(condvar &&o) = default;
+  condvar &operator=(condvar &&o) = default;
 #endif // __cplusplus
 };
