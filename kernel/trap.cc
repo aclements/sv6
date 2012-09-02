5416e9b072dfa0d0ebc42b58d1e2740ef98f7e40
Nickolai Zeldovich <nickolai@csail.mit.edu>
2012-09-02 14:42:48 -0400

    minor

--- kernel/trap.cc
+++ kernel/trap.cc
@@ -24,8 +24,6 @@ extern u64 trapentry[];
 u64
 sysentry_c(u64 a0, u64 a1, u64 a2, u64 a3, u64 a4, u64 a5, u64 num)
 {
-  sti();
-
   if(myproc()->killed) {
     mtstart(trap, myproc());
     exit();
