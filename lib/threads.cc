aa9b6f2a9db905fe6daee3458e1b23f59225e821
Frans Kaashoek <kaashoek@ubuntu.(none)>
2012-10-16 02:33:28 -0400

    implement pthread_mutex using spinlocks

--- lib/threads.cc
+++ lib/threads.cc
@@ -150,3 +150,25 @@ sched_setaffinity(int pid, size_t cpusetsize, cpu_set_t *mask)
   assert(!mask->empty_flag);
   return setaffinity(mask->the_cpu);
 }
+
+int       
+pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr)
+{
+  *mutex = 0;
+  return 0;
+}
+
+int 
+pthread_mutex_lock(pthread_mutex_t *mutex)
+{
+  while(!__sync_bool_compare_and_swap(mutex, 0, 1)) ;
+  return 0;
+}
+
+int 
+pthread_mutex_unlock(pthread_mutex_t *mutex)
+{
+  int b = __sync_bool_compare_and_swap(mutex, 1, 0);
+  return !b;
+}
+
