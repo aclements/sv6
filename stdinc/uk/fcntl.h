142d5072906d1ec082b1db0d880b28b98d2a6744
Nickolai Zeldovich <nickolai@csail.mit.edu>
2012-12-09 13:00:47 -0500

    sync fcntl.h values with Linux

--- stdinc/uk/fcntl.h
+++ stdinc/uk/fcntl.h
@@ -4,9 +4,9 @@
 #define O_RDONLY  0x000
 #define O_WRONLY  0x001
 #define O_RDWR    0x002
-#define O_APPEND  0x004
-#define O_CREAT   0x200
-#define O_TRUNC   0x400
+#define O_CREAT   0x040
+#define O_TRUNC   0x200
+#define O_APPEND  0x400
 #define O_WAIT    0x800 // (xv6) open waits for create, read for write
 
 #define AT_FDCWD  -100
