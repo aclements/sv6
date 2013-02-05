2c3e8603435f59ade538e074043075f39743c2d3
Austin Clements <amdragon@mit.edu>
2013-02-05 15:30:25 -0500

    time: User space support for wall clock time
    time, mktime, localtime*, gmtime*, asctime*, ctime*

--- stdinc/time.h
+++ stdinc/time.h
@@ -1,11 +1,18 @@
 #pragma once
-#include "types.h"
-typedef u64 time_t;
 
-static inline time_t
-time(time_t* t)
-{
-  time_t x = 0;
-  if (t) *t = x;
-  return x;
-}
+#include "compiler.h"
+#include <uk/time.h>
+
+BEGIN_DECLS
+
+// See uk/time.h for time math functions shared with the kernel
+
+time_t time(time_t* t);
+struct tm *gmtime(const time_t *timep);
+struct tm *localtime(const time_t *timep);
+char *asctime_r(const struct tm *tm, char *buf);
+char *asctime(const struct tm *tm);
+char *ctime_r(const time_t *timep, char *buf);
+char *ctime(const time_t *timep);
+
+END_DECLS
