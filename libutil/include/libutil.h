aff9c8456fdada23367626793c39beb191d74c44
Austin Clements <amdragon@mit.edu>
2013-02-06 16:22:28 -0500

    libutil: Move stuff from user/util.h into libutil
    Everything that's left in user/util.h is unused outside user/.

--- libutil/include/libutil.h
+++ libutil/include/libutil.h
@@ -7,5 +7,12 @@ BEGIN_DECLS
 // die.c
 void die(const char* errstr, ...)
   __attribute__((noreturn, __format__(__printf__, 1, 2)));
+void edie(const char* errstr, ...)
+  __attribute__((noreturn, __format__(__printf__, 1, 2)));
+
+#if !defined(XV6_USER)
+// setaffinity is a syscall in xv6, but not standard in Linux
+int setaffinity(int c);
+#endif
 
 END_DECLS
--- libutil/libutil.cc
+++ libutil/libutil.cc
@@ -3,17 +3,17 @@
 #include "libutil.h"
 
 #include <stdarg.h>
+#include <errno.h>
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
 
-void __attribute__((noreturn))
-die(const char* errstr, ...)
+static void __attribute__((noreturn))
+vdie(const char* errstr, va_list ap)
 {
-  va_list ap;
-
-  va_start(ap, errstr);
   vfprintf(stderr, errstr, ap);
-  va_end(ap);
   fprintf(stderr, "\n");
 #if defined(XV6_USER)
   exit();
@@ -21,3 +21,43 @@ die(const char* errstr, ...)
   exit(1);
 #endif
 }
+
+void __attribute__((noreturn))
+die(const char* errstr, ...)
+{
+  va_list ap;
+
+  va_start(ap, errstr);
+  vdie(errstr, ap);
+}
+
+void
+edie(const char* errstr, ...)
+{
+  va_list ap;
+
+  va_start(ap, errstr);
+#ifdef XV6_USER
+  // There is no errno on xv6
+  vdie(errstr, ap);
+#else
+  vfprintf(stderr, errstr, ap);
+  va_end(ap);
+  fprintf(stderr, "\n");
+  fprintf(stderr, ": %s\n", strerror(errno));
+  exit(1);
+#endif
+}
+
+#if !defined(XV6_USER)
+int
+setaffinity(int c)
+{
+  cpu_set_t cpuset;
+  CPU_ZERO(&cpuset);
+  CPU_SET(c, &cpuset);
+  if (sched_setaffinity(0, sizeof(cpuset), &cpuset) < 0)
+    edie("setaffinity, sched_setaffinity failed");
+  return 0;
+}
+#endif
