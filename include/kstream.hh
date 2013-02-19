6d9f13b6001aec8d47f8c28563bed43cbf514c56
Austin Clements <amdragon@mit.edu>
2013-02-19 03:03:45 -0500

    kstream: Printer for windowed buffers

--- include/kstream.hh
+++ include/kstream.hh
@@ -2,6 +2,8 @@
 
 #include "pstream.hh"
 
+#include <string.h>
+
 class console_stream : public print_stream
 {
 public:
@@ -34,6 +36,54 @@ public:
 protected:
   bool begin_print();
   void end_print();
+};
+
+// A stream that captures a window of its output in a buffer.  This is
+// intended for writing simple device read functions where the O(n^2)
+// behavior of printing the entire output each time isn't a problem.
+class window_stream : public print_stream
+{
+  char *out_;
+  // The start and limit of the window to write to out_.
+  size_t start_, lim_;
+  // The number of bytes of output consumed, up to lim_.
+  size_t cur_;
+
+public:
+  window_stream(char *out, size_t pos, size_t n)
+    : out_(out), start_(pos), lim_(pos + n), cur_(0) { }
+
+  // Return the number of bytes used in the output buffer.
+  size_t get_used() const
+  {
+    if (cur_ <= start_)
+      return 0;
+    return cur_ - start_;
+  }
+
+protected:
+  void write(sbuf buf)
+  {
+    if (cur_ < start_) {
+      size_t skip = start_ - cur_;
+      if (buf.len < skip) {
+        cur_ += buf.len;
+        return;
+      }
+      buf.base += skip;
+      buf.len -= skip;
+      cur_ += skip;
+    }
+    size_t copy = buf.len;
+    if (copy > lim_ - cur_)
+      copy = lim_ - cur_;
+    if (copy != 0) {
+      memmove(out_ + (cur_ - start_), buf.base, copy);
+      cur_ += copy;
+    }
+    // XXX It would be nice if this could indicate to the caller when
+    // to stop printing.
+  }
 };
 
 extern console_stream console;
