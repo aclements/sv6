6173111d5030800d330934bac0477094c81f8cd3
Nickolai Zeldovich <nickolai@csail.mit.edu>
2013-02-24 00:23:42 -0500

    bin/avar.cc: add an "onkern" option

--- bin/avar.cc
+++ bin/avar.cc
@@ -11,8 +11,10 @@ main(int ac, char **av)
 {
   if (ac == 2 && strcmp(av[1], "on") == 0)
     mtenable_type(mtrace_record_ascope, "xv6-asharing");
+  else if (ac == 2 && strcmp(av[1], "onkern") == 0)
+    mtenable_type(mtrace_record_kernelscope, "xv6-asharing");
   else if (ac == 2 && strcmp(av[1], "off") == 0)
     mtdisable("xv6-asharing");  
   else
-    die("usage: %s on|off\n", av[0]);
+    die("usage: %s on|onkern|off\n", av[0]);
 }
