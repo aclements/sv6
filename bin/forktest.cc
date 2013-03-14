c8f2b2d7ecf3671bbebde0d91f13328dc9c0afff
Frans Kaashoek <kaashoek@ubuntu.(none)>
2013-03-13 20:21:12 -0400

    Fork benchmark that emulates what mailbench does Chen Yu's changes to bin/Makefrag

---
+++ bin/forktest.cc
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "amd64.h"
+#include "xsys.h"
+#if !defined(XV6_USER)
+#include <sys/wait.h>
+#else
+#include "types.h"
+#include "user.h"
+#endif
+
+int nfork;
+
+void child()
+{
+  for (int i = 0; i < nfork; i++) {
+    int pid = xfork();
+    if (pid < 0) {
+      die("fork in child failed\n");
+    }
+    if (pid == 0) {
+      exit(0);
+    } else {
+      if (wait(NULL) < 0) {
+        die("wait in child failed\n");
+      }
+    }
+  }
+}
+
+int
+main(int argc, char *argv[])
+{
+  int ncore;
+  ncore = atoi(argv[1]);
+  nfork = atoi(argv[2]);
+
+  for (int i = 0; i < ncore; i++) {
+    int pid = xfork();
+    if (pid < 0) {
+      die("fork failed");
+    }
+    if (pid == 0) {
+      child();
+      exit(0);
+    }
+  }
+
+  printf("parent waits\n");
+
+  for (int i = 0; i < ncore; i++) {
+    wait(NULL);
+  }
+  
+  printf("all children done\n");
+
+  return 0;
+}
