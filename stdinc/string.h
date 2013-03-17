8a5a14e2594961b42c9b28e1181ef191bf40e4a9
Nickolai Zeldovich <nickolai@csail.mit.edu>
2013-03-17 01:09:28 -0400

    mtrace build fix

--- stdinc/string.h
+++ stdinc/string.h
@@ -4,17 +4,23 @@
 #include "compiler.h"
 #include <stddef.h>
 
+#ifdef __cplusplus
+#define XV6_STRING_NOEXCEPT noexcept
+#else
+#define XV6_STRING_NOEXCEPT
+#endif
+
 BEGIN_DECLS
 
-void *memcpy(void *s1, const void *s2, size_t n);
-void *memmove(void *s1, const void *s2, size_t n);
+void *memcpy(void *s1, const void *s2, size_t n) XV6_STRING_NOEXCEPT;
+void *memmove(void *s1, const void *s2, size_t n) XV6_STRING_NOEXCEPT;
 int memcmp(const void *s1, const void *s2, size_t n);
 void *memset(void *s, int c, size_t n);
 
 char *strchr(const char *s, int c);
 size_t strlen(const char *s);
 char* strcpy(char*, const char*);
-char* strncpy(char *s, const char *t, size_t n);
+char* strncpy(char *s, const char *t, size_t n) XV6_STRING_NOEXCEPT;
 int strcmp(const char*, const char*);
 int strncmp(const char *p, const char *q, size_t n);
 char *strstr(const char *haystack, const char *needle);
