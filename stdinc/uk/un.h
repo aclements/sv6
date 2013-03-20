fb6484297234a92d937c92d5010b41c986bf5757
Austin Clements <amdragon@mit.edu>
2013-03-20 16:38:33 -0400

    sockets: Make sure sun_family is at the right offset
    LWIP puts some extra fields before the family, so ensure that
    sockaddr_un::sun_family is in the same place as sa_family.

--- stdinc/uk/un.h
+++ stdinc/uk/un.h
@@ -4,6 +4,11 @@
 
 struct sockaddr_un
 {
+  // Make sure ss_family is in the right place.  LWIP has fields
+  // before the family.
+  char __pad0[offsetof(struct sockaddr, sa_family)]
+  __attribute__((__aligned__(__alignof(struct sockaddr))));
+
   sa_family_t sun_family;
   char sun_path[UNIX_PATH_MAX];
 };
