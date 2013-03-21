7f4567120734d63b602b67776638fcc89d2a4bfd
Austin Clements <amdragon@mit.edu>
2013-03-20 20:42:18 -0400

    lib: User prototypes for connect, send, sendto, recv, recvfrom

--- stdinc/sys/socket.h
+++ stdinc/sys/socket.h
@@ -7,7 +7,14 @@ BEGIN_DECLS
 
 int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
 int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
 int listen(int sockfd, int backlog);
+ssize_t send(int sockfd, const void *msg, size_t len, int flags);
+ssize_t sendto(int sockfd, const void *msg, size_t len, int flags,
+               const struct sockaddr *dest_addr, socklen_t addrlen);
 int socket(int domain, int type, int protocol);
+ssize_t recv(int sockfd, void *buf, size_t len, int flags);
+ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
+                 struct sockaddr *src_addr, socklen_t *addrlen);
 
 END_DECLS
