fdedf6c16c513ee85c283777e1c7b7e936979239
Austin Clements <amdragon@mit.edu>
2013-03-26 20:05:58 -0400

    mail-qman: Argument to control alternate API use

--- bin/mail-qman.cc
+++ bin/mail-qman.cc
@@ -7,16 +7,12 @@
 // * notify - a UNIX socket that receives an <inumber> when a message
 //   is added to the spool
 
-#define HAVE_POSIX_SPAWN 1
-
 #include "libutil.h"
 #include "shutil.h"
 #include "xsys.h"
 
 #include <fcntl.h>
-#if HAVE_POSIX_SPAWN
 #include <spawn.h>
-#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -34,6 +30,8 @@ using std::thread;
 
 extern char **environ;
 
+static bool alt;
+
 class spool_reader
 {
   string spooldir_;
@@ -44,8 +42,7 @@ public:
   spool_reader(const string &spooldir) : spooldir_(spooldir)
   {
     // Create notification socket
-    // XXX Commutativity: Unordered
-    notifyfd_ = socket(AF_UNIX, SOCK_DGRAM_UNORDERED, 0);
+    notifyfd_ = socket(AF_UNIX, alt ? SOCK_DGRAM_UNORDERED : SOCK_DGRAM, 0);
     if (notifyfd_ < 0)
       edie("socket failed");
     struct sockaddr_un sun{};
@@ -79,12 +76,14 @@ public:
   {
     char path[256];
     snprintf(path, sizeof path, "%s/todo/%s", spooldir_.c_str(), id.c_str());
-    // XXX Commutativity: O_ANYFD
-    int fd = open(path, O_RDONLY|O_CLOEXEC|O_ANYFD);
+    int fd = open(path, O_RDONLY|O_CLOEXEC|(alt ? O_ANYFD : 0));
     if (fd < 0)
       edie("open %s failed", path);
     struct stat st;
-    // XXX Commutativity: STAT_OMIT_NLINK
+    // We don't use "alt" here, even though this is an alternate
+    // interface, because this commutes regardless.  Passing
+    // STAT_OMIT_NLINK is just for performance.
+    // XXX Maybe we should use a resizing read rather than fstat.
     if (fstatx(fd, &st, STAT_OMIT_NLINK) < 0)
       edie("fstat %s failed", path);
     string res(st.st_size, 0);
@@ -98,8 +97,7 @@ public:
   {
     char path[256];
     snprintf(path, sizeof path, "%s/mess/%s", spooldir_.c_str(), id.c_str());
-    // XXX Commutativity: O_ANYFD
-    int fd = open(path, O_RDONLY|O_CLOEXEC|O_ANYFD);
+    int fd = open(path, O_RDONLY|O_CLOEXEC|(alt ? O_ANYFD : 0));
     if (fd < 0)
       edie("open %s failed", path);
     return fd;
@@ -145,33 +143,32 @@ deliver(const char *mailroot, int msgfd, const string &recipient)
 {
   const char *argv[] = {"./mail-deliver", mailroot, recipient.c_str(), nullptr};
 
-  // XXX Commutativity: fork/exec vs posix_spawn
   pid_t pid;
-#if HAVE_POSIX_SPAWN
 #if defined(XV6_USER)
   // xv6 doesn't define errno.
   int errno = 0;
 #endif
-  posix_spawn_file_actions_t actions;
-  if ((errno = posix_spawn_file_actions_init(&actions)))
-    edie("posix_spawn_file_actions_init failed");
-  if ((errno = posix_spawn_file_actions_adddup2(&actions, msgfd, 0)))
-    edie("posix_spawn_file_actions_adddup2 failed");
-  if ((errno = posix_spawn(&pid, argv[0], &actions, nullptr,
-                           const_cast<char *const*>(argv), environ)))
-    edie("posix_spawn failed");
-  if ((errno = posix_spawn_file_actions_destroy(&actions)))
-    edie("posix_spawn_file_actions_destroy failed");
-#else
-  pid = xfork();
-  if (pid < 0)
-    edie("fork failed");
-  if (pid == 0) {
-    dup2(msgfd, 0);
-    execv(argv[0], const_cast<char *const*>(argv));
-    edie("execv %s failed", argv[0]);
+  if (alt) {
+    posix_spawn_file_actions_t actions;
+    if ((errno = posix_spawn_file_actions_init(&actions)))
+      edie("posix_spawn_file_actions_init failed");
+    if ((errno = posix_spawn_file_actions_adddup2(&actions, msgfd, 0)))
+      edie("posix_spawn_file_actions_adddup2 failed");
+    if ((errno = posix_spawn(&pid, argv[0], &actions, nullptr,
+                             const_cast<char *const*>(argv), environ)))
+      edie("posix_spawn failed");
+    if ((errno = posix_spawn_file_actions_destroy(&actions)))
+      edie("posix_spawn_file_actions_destroy failed");
+  } else {
+    pid = xfork();
+    if (pid < 0)
+      edie("fork failed");
+    if (pid == 0) {
+      dup2(msgfd, 0);
+      execv(argv[0], const_cast<char *const*>(argv));
+      edie("execv %s failed", argv[0]);
+    }
   }
-#endif
 
   int status;
   if (waitpid(pid, &status, 0) < 0)
@@ -202,19 +199,37 @@ do_process(spool_reader *spool, const char *mailroot, int nthread, int cpu)
 static void
 usage(const char *argv0)
 {
-  fprintf(stderr, "Usage: %s spooldir mailroot nthread\n", argv0);
+  fprintf(stderr, "Usage: %s [options] spooldir mailroot nthread\n", argv0);
+  fprintf(stderr, "  -a none   Use regular APIs (default)\n");
+  fprintf(stderr, "     all    Use alternate APIs\n");
   exit(2);
 }
 
 int
 main(int argc, char **argv)
 {
-  if (argc != 4)
+  int opt;
+  while ((opt = getopt(argc, argv, "a:")) != -1) {
+    switch (opt) {
+    case 'a':
+      if (strcmp(optarg, "all") == 0)
+        alt = true;
+      else if (strcmp(optarg, "none") == 0)
+        alt = false;
+      else
+        usage(argv[0]);
+      break;
+    default:
+      usage(argv[0]);
+    }
+  }
+
+  if (argc - optind != 3)
     usage(argv[0]);
 
-  const char *spooldir = argv[1];
-  const char *mailroot = argv[2];
-  int nthread = atoi(argv[3]);
+  const char *spooldir = argv[optind];
+  const char *mailroot = argv[optind+1];
+  int nthread = atoi(argv[optind+2]);
   if (nthread <= 0)
     usage(argv[0]);
 
