cb55d08eaab47b15cd76a3e990d4477ca3fdc75c
Frans Kaashoek <kaashoek@ubuntu.(none)>
2013-04-29 11:16:38 -0400

    conceptually better to return -1

--- kernel/syscall.cc
+++ kernel/syscall.cc
@@ -131,7 +131,7 @@ syscall(u64 a0, u64 a1, u64 a2, u64 a3, u64 a4, u64 a5, u64 num)
       gc_wakeup();
       yield();
     } catch (KillException &e) {
-      return EINTR;
+      return -1;
     }
 #endif
   }
