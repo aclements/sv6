e132a804bdd9cf310084cbc70494fbdc9ea43c19
Nickolai Zeldovich <nickolai@csail.mit.edu>
2013-07-15 22:40:03 -0400

    xv6 stubs to build fstest

--- stdinc/uk/signal.h
+++ stdinc/uk/signal.h
@@ -2,9 +2,11 @@
 
 #define SIGBUS    7
 #define SIGSEGV   11
-#define NSIG      12
+#define SIGPIPE   13
+#define NSIG      16
 
 #define SIG_DFL   ((void (*)(int)) 0)
+#define SIG_IGN   ((void (*)(int)) 1)
 #define SIG_ERR   ((void (*)(int)) -1)
 
 struct sigaction {
