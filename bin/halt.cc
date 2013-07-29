1af96543c242c84dd490656bec60d4d3806722f2
Austin Clements <amdragon@mit.edu>
2013-07-28 21:25:58 -0400

    halt: Support HW=linux

--- bin/halt.cc
+++ bin/halt.cc
@@ -1,9 +1,20 @@
-#include "types.h"
+#ifdef XV6_USER
 #include "user.h"
+#else
+#include <stdio.h>
+#include <unistd.h>
+#include <linux/reboot.h>
+#include <sys/reboot.h>
+#endif
 
 int
 main(int argc, char *argv[])
 {
+#ifdef XV6_USER
   halt();
+#else
+  reboot(LINUX_REBOOT_CMD_POWER_OFF);
+  perror("reboot failed");
+#endif
   return 0;
 }
