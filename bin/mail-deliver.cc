eeec741f698107f5dbece2685e9cb5935124666e
Austin Clements <amdragon@mit.edu>
2014-03-13 16:06:40 -0400

    mail-deliver: Put maildir logic in a class

--- bin/mail-deliver.cc
+++ bin/mail-deliver.cc
@@ -12,6 +12,50 @@
 
 using std::string;
 
+class maildir_writer
+{
+  string maildir_;
+  unsigned long seqno_;
+
+public:
+  maildir_writer(const string &maildir) : maildir_(maildir), seqno_(0)
+  {
+    // Check for mailbox
+    struct stat st;
+    if (stat(maildir.c_str(), &st) < 0 ||
+        (st.st_mode & S_IFMT) != S_IFDIR)
+      die("No such mailbox: %s", maildir.c_str());
+  }
+
+  void deliver(int msgfd)
+  {
+    // Generate unique tmp path
+    char unique[16];
+    snprintf(unique, sizeof(unique), "%d.%lu", getpid(), seqno_);
+    string tmppath(maildir_);
+    tmppath.append("/tmp/").append(unique);
+    ++seqno_;
+
+    // Write message
+    int fd = open(tmppath.c_str(), O_CREAT|O_EXCL|O_WRONLY, 0600);
+    if (fd < 0)
+      edie("open %s failed", tmppath.c_str());
+    if (copy_fd(fd, 0) < 0)
+      edie("copy_fd failed");
+    struct stat st;
+    if (fstatx(fd, &st, STAT_OMIT_NLINK) < 0)
+      edie("fstat %s failed", tmppath.c_str());
+    close(fd);
+
+    // Deliver message
+    snprintf(unique, sizeof(unique), "%lu", (unsigned long)st.st_ino);
+    string newpath(maildir_);
+    newpath.append("/new/").append(unique);
+    if (rename(tmppath.c_str(), newpath.c_str()) < 0)
+      edie("rename %s %s failed", tmppath.c_str(), newpath.c_str());
+  }
+};
+
 static void
 usage(const char *argv0)
 {
@@ -28,36 +72,13 @@ main(int argc, char **argv)
   const char *mailroot = argv[1];
   const char *user = argv[2];
 
-  // Check user's mailbox
+  // Get user's mailbox
   string maildir(mailroot);
   maildir.append("/").append(user);
-  struct stat st;
-  if (stat(maildir.c_str(), &st) < 0 ||
-      (st.st_mode & S_IFMT) != S_IFDIR)
-    die("No such mailbox: %s", maildir.c_str());
-
-  // Generate unique tmp path
-  char unique[16];
-  snprintf(unique, sizeof(unique), "%d", getpid());
-  string tmppath(maildir);
-  tmppath.append("/tmp/").append(unique);
-
-  // Write message
-  int fd = open(tmppath.c_str(), O_CREAT|O_EXCL|O_WRONLY, 0600);
-  if (fd < 0)
-    edie("open %s failed", tmppath.c_str());
-  if (copy_fd(fd, 0) < 0)
-    edie("copy_fd failed");
-  if (fstatx(fd, &st, STAT_OMIT_NLINK) < 0)
-    edie("fstat %s failed", tmppath.c_str());
-  close(fd);
+  maildir_writer writer{maildir};
 
   // Deliver message
-  snprintf(unique, sizeof(unique), "%lu", (unsigned long)st.st_ino);
-  string newpath(maildir);
-  newpath.append("/new/").append(unique);
-  if (rename(tmppath.c_str(), newpath.c_str()) < 0)
-    edie("rename %s %s failed", tmppath.c_str(), newpath.c_str());
+  writer.deliver(0);
 
   return 0;
 }
