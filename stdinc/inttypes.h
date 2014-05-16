bcf52546bb26d9a55c18ef7b1a04091c9a28e638
Austin Clements <amdragon@mit.edu>
2014-05-16 01:46:15 -0400

    stdinc: Figure out __WORDSIZE if necessary

--- stdinc/inttypes.h
+++ stdinc/inttypes.h
@@ -1,12 +1,24 @@
 #pragma once
 #include <stdint.h>
 
+// In freestanding mode, we don't get __WORDSIZE.  This is how
+// bits/wordsize.h figures it out.
+#ifndef __WORDSIZE
+# if defined __x86_64__ && !defined __ILP32__
+#  define __WORDSIZE     64
+# else
+#  define __WORDSIZE     32
+# endif
+#endif
+
 #if __WORDSIZE == 64
 # define __PRI64_PREFIX        "l"
 # define __PRIPTR_PREFIX       "l"
-#else
+#elif __WORDSIZE == 32
 # define __PRI64_PREFIX        "ll"
 # define __PRIPTR_PREFIX
+#else
+# error Unexpected __WORDSIZE
 #endif
 
 #define PRIu32 "u"
