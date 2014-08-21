4b0825a08cfbdf29b243c3ebcb80e6c7e7312b5a
Austin Clements <amdragon@mit.edu>
2014-08-21 11:32:47 -0400

    param: Increase kernel stack size to 32K
    Because 32K should be enough for anyone.  We do have several large
    stack users; mostly those that keep disk blocks on the stack (which is
    probably bad).  The worst of these requires about 16K of stack.

--- param.h
+++ param.h
@@ -1,6 +1,6 @@
 #pragma once
 #define NPROC        64  // maximum number of processes
-#define KSTACKSIZE 8192  // size of per-process kernel stack
+#define KSTACKSIZE 32768 // size of per-process kernel stack
 #define NOFILE      100  // open files per process
 #define NFILE       100  // open files per system
 #define NBUF      10000  // size of disk block cache
