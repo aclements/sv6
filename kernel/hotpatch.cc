e53fb5742d12c90b74d7cddf51e86517ef2e327a
Jonathan Behrens <fintelia@gmail.com>
2019-11-11 13:44:10 -0500

    Hotpatch code should work even if some retpoline thunks are missing
    This is needed because different versions of GCC seem to provide retpolines for
    different sets of registers.

--- kernel/hotpatch.cc
+++ kernel/hotpatch.cc
@@ -2,39 +2,41 @@
 #include "types.h"
 #include "kernel.hh"
 
-void* qtext;
+#define REPLACE(symbol, replacement) \
+  extern u64 symbol; \
+  replace_qtext(&symbol, replacement);
 
-extern u64 __x86_indirect_thunk_rax;
-extern u64 __x86_indirect_thunk_rcx;
-extern u64 __x86_indirect_thunk_rdx;
-extern u64 __x86_indirect_thunk_rbx;
-extern u64 __x86_indirect_thunk_rsi;
-extern u64 __x86_indirect_thunk_r8;
-extern u64 __x86_indirect_thunk_r9;
-extern u64 __x86_indirect_thunk_r10;
-extern u64 __x86_indirect_thunk_r12;
-extern u64 __x86_indirect_thunk_r13;
-extern u64 __x86_indirect_thunk_r14;
-extern u64 __x86_indirect_thunk_r15;
+void* qtext;
 
 void replace_qtext(void* target, const char* value)
 {
+  // Since we can't detect whether a symbol exists, we instead have kernel.ld
+  // add non-present symbols with a value of zero.
+  if(!target)
+    return;
+
   for(char* p = (char*)target - KTEXT + (u64)qtext; *value; p++, value++)
     *p = *value;
 }
 
 void remove_retpolines()
 {
-    replace_qtext(&__x86_indirect_thunk_rax, "\xff\xe0");
-    replace_qtext(&__x86_indirect_thunk_rcx, "\xff\xe1");
-    replace_qtext(&__x86_indirect_thunk_rdx, "\xff\xe2");
-    replace_qtext(&__x86_indirect_thunk_rbx, "\xff\xe3");
-    replace_qtext(&__x86_indirect_thunk_rsi, "\xff\xe6");
-    replace_qtext(&__x86_indirect_thunk_r8, "\x41\xff\xe0");
-    replace_qtext(&__x86_indirect_thunk_r9, "\x41\xff\xe1");
-    replace_qtext(&__x86_indirect_thunk_r10, "\x41\xff\xe2");
-    replace_qtext(&__x86_indirect_thunk_r12, "\x41\xff\xe4");
-    replace_qtext(&__x86_indirect_thunk_r13, "\x41\xff\xe5");
+    REPLACE(__x86_indirect_thunk_rax, "\xff\xe0");
+    REPLACE(__x86_indirect_thunk_rcx, "\xff\xe1");
+    REPLACE(__x86_indirect_thunk_rdx, "\xff\xe2");
+    REPLACE(__x86_indirect_thunk_rbx, "\xff\xe3");
+    REPLACE(__x86_indirect_thunk_rsp, "\xff\xe4");
+    REPLACE(__x86_indirect_thunk_rbp, "\xff\xe5");
+    REPLACE(__x86_indirect_thunk_rsi, "\xff\xe6");
+    REPLACE(__x86_indirect_thunk_rdi, "\xff\xe7");
+    REPLACE(__x86_indirect_thunk_r8, "\x41\xff\xe0");
+    REPLACE(__x86_indirect_thunk_r9, "\x41\xff\xe1");
+    REPLACE(__x86_indirect_thunk_r10, "\x41\xff\xe2");
+    REPLACE(__x86_indirect_thunk_r11, "\x41\xff\xe3");
+    REPLACE(__x86_indirect_thunk_r12, "\x41\xff\xe4");
+    REPLACE(__x86_indirect_thunk_r13, "\x41\xff\xe5");
+    REPLACE(__x86_indirect_thunk_r14, "\x41\xff\xe6");
+    REPLACE(__x86_indirect_thunk_r15, "\x41\xff\xe7");
 }
 
 void inithotpatch()
