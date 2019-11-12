96f95c87b594f509908e9e5e31c1e48e54afdd80
Anton-Cao <cao.anton@gmail.com>
2019-11-12 01:16:47 -0500

    parse cmdline for pcid support

--- kernel/cmdline.cc
+++ kernel/cmdline.cc
@@ -10,6 +10,8 @@
 
 extern char cmdline[];
 
+struct cmdline_params cmdline_params;
+
 static int
 cmdlineread(mdev*, char *dst, u32 off, u32 n)
 {
@@ -23,11 +25,29 @@ cmdlineread(mdev*, char *dst, u32 off, u32 n)
   return cc;
 }
 
+// parse cmdline to populate global cmdline_params struct
+void
+parsecmdline(void)
+{
+  /*
+  std::string str(cmdline);
+  cmdline_params.has_pcid_support =
+    (str.find("has-pcid-support")
+     != std::string::npos);
+  */
+  cmdline_params.has_pcid_support = (strstr(cmdline, "has-pcid-support") != NULL);
+}
+
 void
 initcmdline(void)
 {
   if (VERBOSE)
     cprintf("cmdline: %s\n", cmdline);
+
+  cprintf("\n\n");
+  cprintf("cmdline: %s\n", cmdline);
+
+  parsecmdline();
 
   devsw[MAJ_CMDLINE].pread = cmdlineread;
 }
