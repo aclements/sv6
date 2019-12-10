0a046619de9be696850f8d8fc79a9b64d6d9c64f
Jonathan Behrens <fintelia@gmail.com>
2019-12-10 18:45:11 -0500

    getpid should report timing information

--- bin/getpid.c
+++ bin/getpid.c
@@ -1,16 +1,23 @@
-#include <fcntl.h>
+#include "types.h"
+#include "user.h"
+#include "amd64.h"
+#include "lib.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-#include "libutil.h"
+#define ITERATIONS 10000000UL
 
 int
 main(int argc, char *argv[])
 {
-  for(int i = 0; i < 10000000; i++) {
+  u64 start = rdtsc();
+  for(int i = 0; i < ITERATIONS; i++) {
     getpid();
   }
-  printf("%d\n", getpid());
+  u64 end = rdtsc();
+  printf("%lu ns/iter\n", ((end - start) / ITERATIONS) * 1000000000UL / cpuhz());
   return 0;
 }
