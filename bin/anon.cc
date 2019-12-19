bb7abc7ea3880a53a5bcdfee5f74e8820f46e078
Jonathan Behrens <fintelia@gmail.com>
2019-12-19 15:43:38 -0500

    Port anon microbenchmark to run on Linux as well

--- bin/anon.cc
+++ bin/anon.cc
@@ -1,13 +1,9 @@
-#include "types.h"
-#include "user.h"
-#include "amd64.h"
-#include "lib.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <time.h>
 
 #define ITERATIONS 100000UL
 #define PGSIZE 4096
@@ -15,18 +11,20 @@
 int
 main(int argc, char *argv[])
 {
-  u64 start = rdtsc();
+  struct timespec start, end;
+  clock_gettime(CLOCK_REALTIME, &start);
   for(int i = 0; i < ITERATIONS; i++) {
     void* p = NULL;
     if ((p = mmap(NULL, PGSIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0)) == MAP_FAILED)
-      die("mmap failed");
+      printf("mmap failed");
 
-    *(u64*)p = 12;
+    *(unsigned long*)p = 12;
 
     if (munmap(p, PGSIZE) < 0)
-      die("munmap failed");
+      printf("munmap failed");
   }
-  u64 end = rdtsc();
-  printf("%lu ns/iter\n", ((end - start) / ITERATIONS) * 1000000000UL / cpuhz());
+  clock_gettime(CLOCK_REALTIME, &end);
+  unsigned long long delta = (end.tv_sec - start.tv_sec) * 1000000000UL + (unsigned long long)end.tv_nsec - (unsigned long long)start.tv_nsec;
+  printf("%lu ns/iter\n", delta / ITERATIONS);
   return 0;
 }
