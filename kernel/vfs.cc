c621c1010988fb78d31528c08886e03f28d6f3f8
Cel A. Skeggs <cela@mit.edu>
2020-01-28 15:47:51 -0500

    Disable fat32 mounting
    The support added only works when a FAT32 disk is inserted, which isn't
    always the case. Make it stop trying to mount by default.

--- kernel/vfs.cc
+++ kernel/vfs.cc
@@ -38,10 +38,12 @@ initvfs()
   if (r)
     panic("nullfs mount result: %d\n", r);
 
+#if 0
   auto fat32node = mounts->root()->create_dir("fat32");
   r = mounts->mount(fat32node, vfs_new_fat32(disk_find("ahci0.0p1")));
   if (r)
     panic("fat32 mount result: %d\n", r);
+#endif
 }
 
 int
