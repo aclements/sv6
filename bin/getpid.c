19c48fb942ac1972edee743e92a585a76c7c57ff
Jonathan Behrens <fintelia@gmail.com>
2020-02-11 11:32:42 -0500

    Make gitpid benchmark runnable on Linux

--- bin/getpid.c
+++ bin/getpid.c
@@ -1,23 +1,23 @@
-#include "types.h"
-#include "user.h"
-#include "amd64.h"
-#include "lib.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <time.h>
 
 #define ITERATIONS 10000000UL
 
 int
 main(int argc, char *argv[])
 {
-  u64 start = rdtsc();
+  struct timespec start, end;
+  clock_gettime(CLOCK_REALTIME, &start);
   for(int i = 0; i < ITERATIONS; i++) {
     getpid();
   }
-  u64 end = rdtsc();
-  printf("%lu ns/iter\n", ((end - start) / ITERATIONS) * 1000000000UL / cpuhz());
+  clock_gettime(CLOCK_REALTIME, &end);
+  unsigned long delta = (end.tv_sec - start.tv_sec) * 1000000000UL + (unsigned long)end.tv_nsec
+    - (unsigned long)start.tv_nsec;
+  printf("%lu ns/iter\n", delta / ITERATIONS);
   return 0;
 }
